111be62988f372caee662b2b7f90b5a55f95252b
My Name <cpgsom819@gmail.com>
2023-06-28 22:20:39 +0800

    refernce of a pointer

---
+++ refofpointer.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void fun(int *p)
+
+//  it will print 20 as i changed  the refence 
+
+{
+    *p =20;
+
+}
+
+
+int main()
+{
+    int val = 10;
+    int *ptr = &val;
+    fun(ptr);
+    cout << val << endl;
+    return 0;
+}
